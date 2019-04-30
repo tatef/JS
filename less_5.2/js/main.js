591313b1661d2314c7339dfcaf089515fa2d04a4
tatef <35034739+tatef@users.noreply.github.com>
2019-04-30 23:32:36 +0300

    простое 5.2

---
+++ less_5.2/js/main.js
@@ -0,0 +1,27 @@
+let startBtn = document.getElementById('start'),
+budgetVaLue = document.getEIementsEyClassName('budget—value')[0],
+dayEudgetValue = document.getElementsByClassName('daybudget—value')[0],
+levelValue = document.getElementsByClassName('level—value')[0],
+expensesvalue = document.getElementsEyClassName('expenses—value') [0],
+optionalExpensesValue = document.getElementsByClassName('optionalexpenses—Value')[0],
+incomeValue = document.getElementsByClassName('income—value')[0],
+monthsavingsValue = document.getElementsByClassName('monthsavings—value')[0],
+yearSavingsValue = document.getElementsByCLassName('yearsavings—value' ) [0],
+
+expensesItem = document.getElementsEyClassName('expenses—item'),
+expensesatn = document.getElementsayTagName('button') [0],
+optionalExpensesEtn = document.getElementsByTagName('button') [1] .
+countatn = document.getElementsByTagName('button') [2]
+optianalExpensesItem = document.querySelectorAIU ('.optionalexpenses—item').
+incomeItem = document.querySelector('.choose—income'),
+checksavings = document.querySelector('tsavings'),
+sumValue = document.querySelector(' .choose—sum'),
+percentVaLue = document.querysalectod('.choose-percent'),
+yeanlalue = document.querySelector('.year—value'),
+monthValue = document.querySelecmr('month—value'),
+dayValue = ducument.querySelectur('.day—value');
+
+
+
+
+
