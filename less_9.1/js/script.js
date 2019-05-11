5947fa51f7171ffc5a4fedd989f3d94c262d5851
tatef <35034739+tatef@users.noreply.github.com>
2019-05-11 16:54:41 +0300

    Add files via upload

--- less_9.1/js/script.js
+++ less_9.1/js/script.js
@@ -74,17 +74,27 @@ window.addEventListener('DOMContentLoaded', function(){
   }
   setClock('timer', deadline);
   //modal
-  let more =document.querySelector('.more'),
-  overlay = document.querySelector('.overlay'),
-  close = document.querySelector('.popup-close');
-  more.addEventListener('click', function(){
-    overlay.style.display = 'block';
-    this.classList.add('more-splash');
-    document.body.style.overflow = 'hidden';
+  let more = document.querySelector(".more"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close"),
+    descriptionBtn = document.querySelectorAll(".description-btn");
+
+  more.addEventListener("click", function() {
+    overlay.style.display = "block";
+    more.classList.add("more-splash");
+    document.body.style.overflow = "hidden";
   });
-  close.addEventListener('click', function () {
-    overlay.style.display = 'none';
-    more.classList.remove('more-splash');
-    document.body.style.overflow = '';
+
+  close.addEventListener("click", function() {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
   });
+
+  descriptionBtn.forEach(descriptionBtn =>
+    descriptionBtn.addEventListener("click", function(){
+      overlay.style.display = "block";
+      more.classList.add("more-splash");
+      document.body.style.overflow = "hidden";
+    }));
 });
