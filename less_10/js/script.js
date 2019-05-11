86a13564f9a3be20fcfeea1662e76bf7b6281f15
tatef <35034739+tatef@users.noreply.github.com>
2019-05-11 20:02:48 +0300

    Add files via upload

--- less_10/js/script.js
+++ less_10/js/script.js
@@ -1,41 +1,100 @@
+window.addEventListener('DOMContentLoaded', function(){
+  'use script';
+  let tab = document.querySelectorAll('.info-header-tab'),
+  info = document.querySelector('.info-header'),
+  tabContent = document.querySelectorAll('.info-tabcontent');
+  function hideTabContent(a){
+    for (i = a; i < tabContent.length; i++){
+      tabContent[i].classList.remove('show');
+      tabContent[i].classList.add('hide');
 
-window.addEventListener("DOMContentLoaded", function () {
-  // 'use strict';
 
-  class Options {
-    constructor(height = 100, width = 200, bg = "red", fontSize = "20px",
-      textAlign = "center") {
-      this.height = height;
-      this.width = width;
-      this.bg = bg;
-      this.fontSize = fontSize;
-      this.textAlign = textAlign;
-    }
-    getElement() {
-      let newDiv = document.createElement('div'),
-        text = "Какой-то текст";
-
-      newDiv.innerHTML = `${text}`;
-      document.body.appendChild(newDiv);
-
-      newDiv.style.cssText =
-        `border: 1px solid black; margin: auto;` +
-        `height: ${this.height}px;` +
-        `width: ${this.width}px;` +
-        `background-color: ${this.bg};` +
-        `font-size: ${this.fontSize}px;` +
-        `text-align: ${this.textAlign};`;
     }
   }
-
-  let input = document.querySelector("#input"),
-    btn = document.querySelector("#btn");
-
-  btn.addEventListener("click", () => {
-    let arr = input.value.split(", ");
-    console.log(arr);
-    let result = new Options(...arr);
-    result.getElement();
+  hideTabContent(1);
+  function showTabContent(b){
+    if (tabContent[b].classList.contains('hide')){
+      tabContent[b].classList.remove('hide');
+      tabContent[b].classList.add('show');
+    }
+  }
+  info.addEventListener('click', function(event){
+    let target = event.target;
+    if(target && target.classList.contains('info-header-tab')){
+      for( let i =0; i < tab.length; i++){
+        if (target == tab[i]){
+          hideTabContent(0);
+          showTabContent(i);
+          break;
+        }
+      }
+    }
   });
-});
 
+  let deadline = '2018-10-21';
+  function getTimeRemaining(endtime){
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+    seconds = Math.floor((t / 1000) % 60),
+    minutes = Math.floor((t / 1000 / 60) % 60),
+    hours = Math.floor((t / (1000*60*60)));
+    return{
+      'totul' : t,
+      'hours' : hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  function setClock(id, endtime){
+    let timer = document.getElementById(id),
+    hours = timer.querySelector('.hours'),
+    minutes = timer.querySelector('.minutes'),
+    seconds = timer.querySelector('.seconds'),
+    timeInterval = setInterval(upDateClock, 1000);
+    function upDateClock(){
+      let t = getTimeRemaining(endtime);
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+      if (t.totul <= 0){
+        clearInterval(timeInterval);
+      }
+      if (t.hours <= 0 && t.minutes <= 0 && t.seconds <= 0 ){
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        seconds.textContent = "00";
+      }
+      function addZero(endtime) {
+        if (endtime >= 0 && endtime < 10) {
+          return '0' + endtime;
+        } else {
+          return endtime;
+        }
+      }
+    }
+  }
+  setClock('timer', deadline);
+  //modal
+  let more = document.querySelector(".more"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close"),
+    descriptionBtn = document.querySelectorAll(".description-btn");
+
+  more.addEventListener("click", function() {
+    overlay.style.display = "block";
+    more.classList.add("more-splash");
+    document.body.style.overflow = "hidden";
+  });
+
+  close.addEventListener("click", function() {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
+  });
+
+  descriptionBtn.forEach(descriptionBtn =>
+    descriptionBtn.addEventListener("click", function(){
+      overlay.style.display = "block";
+      more.classList.add("more-splash");
+      document.body.style.overflow = "hidden";
+    }));
+});
