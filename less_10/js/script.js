f80fb99dab70a0fd67b51483eb30533103c7ed75
tatef <35034739+tatef@users.noreply.github.com>
2019-05-11 23:44:29 +0300

    Add files via upload

--- less_10/js/script.js
+++ less_10/js/script.js
@@ -44,26 +44,26 @@ window.addEventListener("DOMContentLoaded", () => {
       'seconds': seconds
     };
   }
-  const setClock = (id, endtime) => {
+  function setClock(id, endtime) {
     let timer = document.getElementById(id),
-    hours = timer.querySelector('.hours'),
-    minutes = timer.querySelector('.minutes'),
-    seconds = timer.querySelector('.seconds'),
-    timeInterval = setInterval(upDateClock, 1000);
-    const upDateClock = () => {
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(upDateClock, 1000);
+    function upDateClock() {
       let t = getTimeRemaining(endtime);
       hours.textContent = addZero(t.hours);
       minutes.textContent = addZero(t.minutes);
       seconds.textContent = addZero(t.seconds);
-      if (t.totul <= 0){
+      if (t.totul <= 0) {
         clearInterval(timeInterval);
       }
-      if (t.hours <= 0 && t.minutes <= 0 && t.seconds <= 0 ){
+      if (t.hours <= 0 && t.minutes <= 0 && t.seconds <= 0) {
         hours.textContent = "00";
         minutes.textContent = "00";
         seconds.textContent = "00";
       }
-      const addZero = (endtime) => {
+      function addZero(endtime) {
         if (endtime >= 0 && endtime < 10) {
           return '0' + endtime;
         } else {
