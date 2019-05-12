951a5da5ec4683eae79e3b4b19bc2944043585ab
tatef <35034739+tatef@users.noreply.github.com>
2019-05-13 00:11:05 +0300

    Add files via upload

--- less_10/js/script.js
+++ less_10/js/script.js
@@ -3,16 +3,14 @@ window.addEventListener("DOMContentLoaded", () => {
   let tab = document.querySelectorAll('.info-header-tab'),
   info = document.querySelector('.info-header'),
   tabContent = document.querySelectorAll('.info-tabcontent');
-  const hideTabContent = (a) =>{
-    for (i = a; i < tabContent.length; i++){
+  const hideTabContent = (a) => {
+    for (let i = a; i < tabContent.length; i++) {
       tabContent[i].classList.remove('show');
       tabContent[i].classList.add('hide');
-
-
     }
   }
   hideTabContent(1);
-  const showTabContent = (b) =>{
+  const showTabContent = (b) => {
     if (tabContent[b].classList.contains('hide')){
       tabContent[b].classList.remove('hide');
       tabContent[b].classList.add('show');
