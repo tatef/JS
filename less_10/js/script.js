5598ebde6f3a0cd2011d97a7fbf3d19054cf22a1
tatef <35034739+tatef@users.noreply.github.com>
2019-05-12 23:51:08 +0300

    Add files via upload

--- less_10/js/script.js
+++ less_10/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-  //'use strict';
+  'use strict';
   let tab = document.querySelectorAll('.info-header-tab'),
   info = document.querySelector('.info-header'),
   tabContent = document.querySelectorAll('.info-tabcontent');
