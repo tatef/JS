4ae36b7c28be653a74d89d67f455323de4096e23
tatef <35034739+tatef@users.noreply.github.com>
2019-05-18 00:18:17 +0300

    Add files via upload

--- less_12.1/js/script.js
+++ less_12.1/js/script.js
@@ -102,62 +102,84 @@ hideModal();
 
   // FORM
 
+  // FORM
   let message = {
-    loading: "Загрузка...",
-    success: "Спасибо. Cкоро мы с вами свяжемся.",
-    failure: "Что-то пошло не так."
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
   };
+  let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input')[0],
+    statusMessage = document.createElement('div'),
+    formTelEmail = document.getElementById('form'),
+    inputs = formTelEmail.getElementsByTagName('input'),
+    input1 = formTelEmail.getElementsByTagName('input')[0],
+    input2 = formTelEmail.getElementsByTagName('input')[1];
 
-  let form = document.getElementsByClassName("main-form") [0],
-    formBottom = document.getElementById("form"),
-    input = document.getElementsByTagName("input"),
-    statusMessage = document.createElement("div");
-  statusMessage.classList.add("status");
-// promis 
-function sendForm(elem) {
-  elem.addEventListener('submit', function (e) {
-    e.preventDefault();
-    elem.appendChild(statusMessage);
-    let formData = new FormData(elem);
-    let input = elem.querySelectorAll('input');
+  input1.name = 'email';
+  input2.name = 'phone';
 
-    function postData(data) {
-      return new Promise(function (resolve, reject) {
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        request.onreadystatechange = function () {
-          if (request.readyState < 4) {
-            resolve();
-          } else if (request.readyState === 4) {
-            if (request.status == 200 && request.status < 300) {
-              resolve();
-            } else {
-              reject();
-            }
-          }
-        };
-        let obj = {};
-        formData.forEach(function (value, key) {
-          obj[key] = value;
-        });
-        let json = JSON.stringify(obj);
-        request.send(json);
-      });
-    }
-
-    function clearInput() {
-      for (let i = 0; i < input.length; i++) {
-        input[i].value = '';
+  statusMessage.classList.add('status');
+  input2.onkeypress = function (event) {
+    if (event.keyCode != 43) {
+      if (event.keyCode < 48 || event.keyCode > 57) {
+        event.preventDefault();
       }
     }
-    postData(formData)
-      .then(() => statusMessage.innerHTML = message.loading)
-      .then(() => statusMessage.innerHTML = message.success)
-      .catch(() => statusMessage.innerHTML = message.failure)
-      .then(clearInput);
-  });
-}
-sendForm(form);
-sendForm(formTelEmail);
+  };
+  input.onkeypress = function (event) {
+    if (event.keyCode != 43) {
+      if (event.keyCode < 48 || event.keyCode > 57) {
+        event.preventDefault();
+      }
+    }
+  };
+  // promis 
+  function sendForm(elem) {
+    elem.addEventListener('submit', function (e) {
+      e.preventDefault();
+      elem.appendChild(statusMessage);
+      let formData = new FormData(elem);
+      let input = elem.querySelectorAll('input');
+
+      function postData(data) {
+        return new Promise(function (resolve, reject) {
+          let request = new XMLHttpRequest();
+          request.open('POST', 'server.php');
+          request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+          request.onreadystatechange = function () {
+            if (request.readyState < 4) {
+              resolve();
+            } else if (request.readyState === 4) {
+              if (request.status == 200 && request.status < 300) {
+                resolve();
+              } else {
+                reject();
+              }
+            }
+          };
+          let obj = {};
+          formData.forEach(function (value, key) {
+            obj[key] = value;
+          });
+          let json = JSON.stringify(obj);
+          request.send(json);
+        });
+      }
+
+      function clearInput() {
+        for (let i = 0; i < input.length; i++) {
+          input[i].value = '';
+        }
+      }
+      postData(formData)
+        .then(() => statusMessage.innerHTML = message.loading)
+        .then(() => statusMessage.innerHTML = message.success)
+        .catch(() => statusMessage.innerHTML = message.failure)
+        .then(clearInput);
+    });
+  }
+  sendForm(form);
+  sendForm(formTelEmail);
+
 });
