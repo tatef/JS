e09712673b495d21f5edd7724d82e5888ec7440a
tatef <35034739+tatef@users.noreply.github.com>
2019-05-18 03:03:58 +0300

    Add files via upload

---
+++ less_14.1/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+
+  $(".main_btna").on("click", function () {
+    $(".overlay").fadeIn("slow");
+    $(".modal").css({ "top": "-40px", "display": "block", "opacity": "0" });
+    $(".modal").animate({ "top": "30px", "opacity": "1" }, "slow");
+  });
+
+  $(".main_btn").on("click", function () {
+    $(".overlay").fadeIn("slow");
+    $(".modal").css({ "top": "-40px", "display": "block", "opacity": "0" });
+    $(".modal").animate({ "top": "30px", "opacity": "1" }, "slow");
+  });
+
+  $('.main_nav a[href="#sheldure"]').on("click", function () {
+
+    $(".overlay").fadeIn("slow");
+    $(".modal").css({ "top": "-40px", "display": "block", "opacity": "0" });
+    $(".modal").animate({ "top": "30px", "opacity": "1" }, "slow");
+  });
+
+  $(".close").on("click", function () {
+    $(".overlay").fadeToggle("slow");
+    $(".modal").animate({ "top": "-30px", "opacity": "0" }, 500);
+    $(".modal").fadeToggle(500);
+  });
+
+
+
+});
